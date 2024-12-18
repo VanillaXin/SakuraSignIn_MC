ee7146c1ca31903f666ed9b0d1a7cceab3541dcb
Tsuki <TsukiMaaii@gmail.com>
2024-12-18 12:55:02 +0800

    fix 1.玩家进入服务器时同步进度列表导致服务器异常

--- src/main/java/xin/vanilla/mc/network/AdvancementData.java
+++ src/main/java/xin/vanilla/mc/network/AdvancementData.java
@@ -5,7 +5,9 @@ import lombok.NonNull;
 import lombok.experimental.Accessors;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.advancements.FrameType;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -17,51 +19,53 @@ import net.minecraft.world.item.Items;
 @Accessors(chain = true)
 public class AdvancementData {
     @NonNull
-    public final ResourceLocation id;
+    private final ResourceLocation id;
     @NonNull
-    public final String title;
-    @NonNull
-    public final String description;
-    @NonNull
-    public final ItemStack icon;
+    private final DisplayInfo displayInfo;
 
-    public AdvancementData(@NonNull ResourceLocation id, @NonNull String title, @NonNull String description, @NonNull ItemStack icon) {
+    public AdvancementData(@NonNull ResourceLocation id, DisplayInfo displayInfo) {
         this.id = id;
-        this.title = title;
-        this.description = description;
-        this.icon = icon;
-    }
-
-    public void writeToBuffer(FriendlyByteBuf buffer) {
-        buffer.writeResourceLocation(id);
-        buffer.writeUtf(title);
-        buffer.writeUtf(description);
-        buffer.writeItemStack(icon, false);
+        if (displayInfo == null) {
+            this.displayInfo = emptyDisplayInfo();
+        } else {
+            this.displayInfo = displayInfo;
+        }
     }
 
     public static AdvancementData fromAdvancement(Advancement advancement) {
         DisplayInfo displayInfo = advancement.getDisplay();
         if (displayInfo == null) {
-            return new AdvancementData(
-                    advancement.getId(),
-                    advancement.getId().toString(),
-                    "",
-                    new ItemStack(Items.AIR)
-            );
+            return new AdvancementData(advancement.getId(), createDisplayInfo(advancement.getId().toString()));
         }
-        return new AdvancementData(
-                advancement.getId(),
-                displayInfo.getTitle().getString(),
-                displayInfo.getDescription().getString(),
-                displayInfo.getIcon()
-        );
+        return new AdvancementData(advancement.getId(), displayInfo);
     }
 
     public static AdvancementData readFromBuffer(FriendlyByteBuf buffer) {
         ResourceLocation id = buffer.readResourceLocation();
-        String title = buffer.readUtf(32767);
-        String description = buffer.readUtf(32767);
-        ItemStack icon = buffer.readItem();
-        return new AdvancementData(id, title, description, icon);
+        return new AdvancementData(id, DisplayInfo.fromNetwork(buffer));
+    }
+
+    public static DisplayInfo emptyDisplayInfo() {
+        return createDisplayInfo("");
+    }
+
+    public static DisplayInfo createDisplayInfo(String title) {
+        return createDisplayInfo(title, "", new ItemStack(Items.AIR));
+    }
+
+    public static DisplayInfo createDisplayInfo(String title, String description) {
+        return createDisplayInfo(title, description, new ItemStack(Items.AIR));
+    }
+
+    public static DisplayInfo createDisplayInfo(String title, String description, ItemStack itemStack) {
+        return new DisplayInfo(itemStack
+                , Component.literal(title), Component.literal(description)
+                , new ResourceLocation(""), FrameType.TASK
+                , false, false, false);
+    }
+
+    public void writeToBuffer(FriendlyByteBuf buffer) {
+        buffer.writeResourceLocation(id);
+        displayInfo.serializeToNetwork(buffer);
     }
 }
